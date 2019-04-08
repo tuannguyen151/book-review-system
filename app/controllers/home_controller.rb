5121253c1d8ac0ea8b676fe3a7f50bda669be54d
tsn150197 <famaxlevel1997@gmail.com>
2019-04-08 22:54:23 +0700

    User's home page and activities of following user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,10 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    if current_user
+      @activities = Activity.order("created_at DESC")
+                            .where(user_id: current_user.following)
+                            .includes(:activitable, user: [:user_profile])
+                            .page(params[:page]).per Settings.activites
+    end
+  end
 end
