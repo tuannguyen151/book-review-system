05616ce7621b7f65d7cc508c3610f442c3bb1e13
tsn150197 <famaxlevel1997@gmail.com>
2019-06-05 15:34:12 +0700

    API get user

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,4 +1,5 @@
 RSpec.describe Api::V1::UsersController, type: :controller do
+  let(:user) {FactoryBot.create :user, :orther_user}
 
   describe "GET #index" do
     it "returns json success" do
@@ -9,6 +10,24 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     it "returns http success" do
       get :index, format: :json
       expect(response).to have_http_status :success
+    end
+  end
+
+  describe "GET #show" do
+    it "responds to json by default" do
+      get :show, params: {id: user.id, format: :json}
+      expect(response.content_type).to eq "application/json"
+    end
+
+    it "returns http success" do
+      get :show, params: {id: user.id, format: :json}
+      expect(response).to have_http_status :success
+    end
+
+    it "user not found" do
+      get :show, params: {id: 1, format: :json}
+      parse_json = JSON(response.body)
+      expect(parse_json["status"]).to eq 404
     end
   end
 end
